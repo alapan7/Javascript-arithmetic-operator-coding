d9c26ec3af4064921e892883bae1c9f9dd5e94f7
alapan7 <alapanghosh3@gmail.com>
2022-10-04 15:28:22 +0530

    javascript

---
+++ file.js
@@ -0,0 +1,27 @@
+let a=40;
+let b=20;
+let c=a+b;
+let c1=a-b;
+let c2=a*b;
+let c3=a/b;
+let c4=a%b;
+let d=b<a;
+console.log(c,c1,c2,c3,c4,d);
+
+let x=10;
+let y=x;
+x=20;
+console.log(x,y);
+
+let e=10;
+let f=e++;
+console.log(f);
+
+let bb=5;
+let cc=10;
+let dd=cc && bb;
+console.log(dd);
+
+let ss=50;
+let aa=10;
+let i=ss & aa
